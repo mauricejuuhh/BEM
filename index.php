221fb5b578f217521c269aee6f9c366f6b775677
Maurice de Jong <20231852+mauricejuuhh@users.noreply.github.com>
2017-10-10 09:52:04 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <link rel="stylesheet" href="style.css">
+  <body>
+
+    <div id="container">
+    <div class="button button--rood">Ik ben rood</div><div class="button button--blauw">ik ben blauw</div>
+    </div>
+  </body>
+</html>
