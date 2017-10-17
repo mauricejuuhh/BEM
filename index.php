43047fce7eb6c76363962caeb5395d77a6e5a3ee
Maurice de Jong <20231852+mauricejuuhh@users.noreply.github.com>
2017-10-17 10:07:36 +0200

    the thing go's kraa papapapapa

--- index.php
+++ index.php
@@ -6,9 +6,21 @@
   </head>
   <link rel="stylesheet" href="style.css">
   <body>
-
+<nav>
+  <div class="nav--button">klik</div><div class="nav--button">niet</div><div class="nav--button">op</div><div class="nav--button">mij</div>
+</nav>
     <div id="container">
     <div class="button button--rood">Ik ben rood</div><div class="button button--blauw">ik ben blauw</div>
+
+
     </div>
+  <div id="form">
+<form method="post">
+  <input type="text" placeholder="werkt niet">
+  <input type="text" placeholder="werkt niet">
+  <input type="submit" value="-9OI">
+</form>
+
+  </div>
   </body>
 </html>
